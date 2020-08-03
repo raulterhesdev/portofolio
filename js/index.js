9d62997a59553987e99ae0ce5e17113d445ad962
Raul Terhes <raulterhesdevelopment@gmail.com>
2020-08-03 17:48:07 +0300

    Added initial code for loader + Dark/Light Mode

---
+++ js/index.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Delay to show the spinner even if page loads quick
+  for (let i = 0; i < 2000000000; i++) {}
+
+  // Get the spinner and page content
+  loader = document.getElementById('loader');
+  content = document.getElementById('content');
+
+  // Switch from spinner to content when page loads
+  loader.style.display = 'none';
+  content.style.display = 'block';
+
+  // Light/Dark Mode toggle
+  var checkbox = document.querySelector('input[name=mode]');
+
+  checkbox.addEventListener('change', function () {
+    if (this.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+    }
+  });
+});
