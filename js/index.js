6a40cf83cb3ea4f1a5e8a2f2ad65cc749be4b4d5
Raul Terhes <raulterhesdevelopment@gmail.com>
2020-08-11 14:17:44 +0300

    added hero, contact and projects sections

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Delay to show the spinner even if page loads quick
-  for (let i = 0; i < 2000000000; i++) {}
+  // // Delay to show the spinner even if page loads quick
+  // for (let i = 0; i < 2000000000; i++) {}
 
   // Get the spinner and page content
   loader = document.getElementById('loader');
@@ -10,13 +10,68 @@ document.addEventListener('DOMContentLoaded', () => {
   loader.style.display = 'none';
   content.style.display = 'block';
 
-  // Light/Dark Mode toggle
-  var checkbox = document.querySelector('input[name=mode]');
-  checkbox.addEventListener('change', function () {
-    if (this.checked) {
-      document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-      document.documentElement.setAttribute('data-theme', 'light');
+  // Navigation
+  function resetNavigation() {
+    hero.style.display = 'none';
+    projects.style.display = 'none';
+    contact.style.display = 'none';
+    about.style.display = 'none';
+
+    navHero.classList.remove('nav-active');
+    navProjects.classList.remove('nav-active');
+    navContact.classList.remove('nav-active');
+    navAbout.classList.remove('nav-active');
+  }
+
+  function navigate(event) {
+    event.preventDefault();
+    const target = event.currentTarget.href.slice(
+      event.currentTarget.href.indexOf('#') + 1
+    );
+    console.log(target);
+    switch (target) {
+      case 'hero':
+        resetNavigation();
+
+        hero.style.display = 'grid';
+        navHero.classList.add('nav-active');
+        break;
+      case 'projects':
+        resetNavigation();
+
+        projects.style.display = 'block';
+        navProjects.classList.add('nav-active');
+        break;
+      case 'contact':
+        resetNavigation();
+
+        contact.style.display = 'block';
+        navContact.classList.add('nav-active');
+        break;
+      case 'about':
+        resetNavigation();
+
+        about.style.display = 'block';
+        navAbout.classList.add('nav-active');
+        break;
+
+      default:
+        break;
     }
-  });
+  }
+
+  hero = document.getElementById('hero');
+  projects = document.getElementById('projects');
+  contact = document.getElementById('contact');
+  about = document.getElementById('about');
+
+  navHero = document.getElementById('nav-hero');
+  navProjects = document.getElementById('nav-projects');
+  navContact = document.getElementById('nav-contact');
+  navAbout = document.getElementById('nav-about');
+
+  navHero.addEventListener('click', navigate);
+  navProjects.addEventListener('click', navigate);
+  navContact.addEventListener('click', navigate);
+  navAbout.addEventListener('click', navigate);
 });
