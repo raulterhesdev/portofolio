72540e221ea3bf238fbd76465e3ab2f2142657ad
Raul Terhes <raulterhesdevelopment@gmail.com>
2020-09-06 11:53:12 +0300

    code cleanul

--- js/index.js
+++ js/index.js
@@ -10,57 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
   loader.style.display = 'none';
   content.style.display = 'block';
 
-  // Toggle navbar
-  document.querySelector('.toggle').addEventListener('click', (e) => {
-    e.currentTarget.classList.toggle('close');
-    document.querySelector('.link-container').classList.toggle('nav-closed');
-  });
-
-  // Close the navbar on first load if on phone
-  if (window.innerWidth < 621) {
-    document.querySelector('.toggle').classList.toggle('close');
-    document.querySelector('.link-container').classList.toggle('nav-closed');
-  }
-
-  // Navigation
-  function navigate(e) {
-    const target = event.currentTarget.href.slice(
-      event.currentTarget.href.indexOf('#')
-    );
-
-    navHero.classList.remove('nav-active');
-    navProjects.classList.remove('nav-active');
-    navContact.classList.remove('nav-active');
-    navAbout.classList.remove('nav-active');
-
-    // when navigating on a phone close the navbar
-    if (window.innerWidth < 621) {
-      document.querySelector('.toggle').classList.remove('close');
-      document.querySelector('.link-container').classList.add('nav-closed');
-    }
-
-    switch (target) {
-      case '#hero':
-        tlHero.restart();
-        navHero.classList.add('nav-active');
-        break;
-      case '#projects':
-        tlProjects.restart();
-        navProjects.classList.add('nav-active');
-        break;
-      case '#contact':
-        tlContact.restart();
-        navContact.classList.add('nav-active');
-        break;
-      case '#about':
-        tlAbout.restart();
-        navAbout.classList.add('nav-active');
-        break;
-      default:
-        break;
-    }
-  }
-
+  // Get dom elements
   const hero = document.getElementById('hero');
   const projects = document.getElementById('projects');
   const contact = document.getElementById('contact');
@@ -72,13 +22,57 @@ document.addEventListener('DOMContentLoaded', () => {
   const navContact = document.getElementById('nav-contact');
   const navAbout = document.getElementById('nav-about');
 
-  navHero.addEventListener('click', navigate);
-  navProjects.addEventListener('click', navigate);
-  navContact.addEventListener('click', navigate);
-  navAbout.addEventListener('click', navigate);
-  actionButton.addEventListener('click', navigate);
+  const submitButton = document.getElementById('form-submit');
+  const formName = document.getElementById('form-name');
+  const formEmail = document.getElementById('form-email');
+  const formSubject = document.getElementById('form-subject');
+  const formMessage = document.getElementById('form-message');
 
-  window.addEventListener('scroll', function (event) {
+  const nameError = document.getElementById('form-name-error');
+  const emailError = document.getElementById('form-email-error');
+  const subjectError = document.getElementById('form-subject-error');
+  const messageError = document.getElementById('form-message-error');
+
+  const toggle = document.querySelector('.toggle');
+  const linkContainer = document.querySelector('.link-container');
+
+  // Close the navbar on first load if on phone
+  if (window.innerWidth < 621) {
+    toggle.classList.toggle('close');
+    linkContainer.classList.toggle('nav-closed');
+  }
+
+  // Navigation
+  function navigate(e) {
+    const target = event.currentTarget.href.slice(
+      event.currentTarget.href.indexOf('#')
+    );
+
+    // when navigating on a phone close the navbar
+    if (window.innerWidth < 621) {
+      toggle.classList.remove('close');
+      linkContainer.classList.add('nav-closed');
+    }
+
+    switch (target) {
+      case '#hero':
+        tlHero.restart();
+        break;
+      case '#projects':
+        tlProjects.restart();
+        break;
+      case '#contact':
+        tlContact.restart();
+        break;
+      case '#about':
+        tlAbout.restart();
+        break;
+      default:
+        break;
+    }
+  }
+
+  function scrollEvent(event) {
     var scroll = this.scrollY;
     if (scroll < hero.offsetHeight * 0.5) {
       navHero.classList.add('nav-active');
@@ -110,20 +104,9 @@ document.addEventListener('DOMContentLoaded', () => {
       navContact.classList.add('nav-active');
       navAbout.classList.remove('nav-active');
     }
-  });
+  }
 
   // Contact form
-  submitButton = document.getElementById('form-submit');
-  formName = document.getElementById('form-name');
-  formEmail = document.getElementById('form-email');
-  formSubject = document.getElementById('form-subject');
-  formMessage = document.getElementById('form-message');
-
-  nameError = document.getElementById('form-name-error');
-  emailError = document.getElementById('form-email-error');
-  subjectError = document.getElementById('form-subject-error');
-  messageError = document.getElementById('form-message-error');
-
   function submitForm(e) {
     e.preventDefault();
     const name = formName.value;
@@ -168,7 +151,20 @@ document.addEventListener('DOMContentLoaded', () => {
     emailjs.send(service_id, template_id, template_params, emailjsUser);
   }
 
+  navHero.addEventListener('click', navigate);
+  navProjects.addEventListener('click', navigate);
+  navContact.addEventListener('click', navigate);
+  navAbout.addEventListener('click', navigate);
+  actionButton.addEventListener('click', navigate);
+  window.addEventListener('scroll', scrollEvent);
+
   submitButton.addEventListener('click', submitForm);
+
+  // Toggle navbar
+  toggle.addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('close');
+    linkContainer.classList.toggle('nav-closed');
+  });
 
   formName.addEventListener('focus', function (e) {
     formName.classList.remove('input-error');
